f83cd03171fd8e12e3c36c0403c1c4dad11613ff
lggradosm <gabrielgradosmachado@gmail.com>
2025-04-06 10:56:26 -0500

    backend-spring

---
+++ backend-spring/src/test/java/org/notariaguerra/backendspring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.notariaguerra.backendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
